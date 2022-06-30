ab824931e7130136633b2c0ce99a875d1d051f0b
Kaloyan Doychinov <kokolia1070@gmail.com>
2022-06-30 21:12:22 +0300

    [RESTART] Work with acc on lists

--- frontend/src/partials/ToDo/List.jsx
+++ frontend/src/partials/ToDo/List.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaCheck, FaRegTrashAlt, FaFlag } from "react-icons/fa";
 
 const List = () => {
     const [todos, setTodos] = useState(
@@ -20,6 +21,18 @@ const List = () => {
         }
     ]);
 
+/*     const deleteTask = (id) => {
+        fetch(`${process.env.REACT_APP_SERVER_URL}/tasks/${id}`, {
+            method: "DELETE",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("token")}`
+            }
+        })
+        setTodos(todos.filter(todo => todo.id !== id));
+    } */
+
     const toggleTodo = (id) => {
         setTodos(todos.map(todo => {
             if (todo.id === id) {
@@ -37,10 +50,12 @@ const List = () => {
             <h1 className="text-4xl font-bold mb-5">To Do List</h1>
             <ul className="flex flex-col space-y-3 dark:text-white">
                 {todos?.map(todo => (
-                    <li key={todo?.id} className={"flex justify-start items-center space-x-3 p-5 dark:bg-slate-900 border dark:border-slate-800 rounded-xl ".concat((todo?.completed) ? "opacity-80 line-through" : "")}>
-                        <button className="p-2 rounded-md border bg-slate-800 dark:border-slate-700 dark:text-white dark:hover:bg-slate-700 dark:hover:border-slate-600" onClick={() => toggleTodo(todo?.id)}></button>
-                        <span className="text-lg">{todo?.title}</span>
-                        <button className="p-2 rounded-md border bg-slate-800 dark:border-slate-700 dark:text-white dark:hover:bg-slate-700 dark:hover:border-slate-600"></button>
+                    <li key={todo?.id} className={"flex justify-between items-center p-5 dark:bg-slate-900 border dark:border-slate-800 rounded-xl ".concat((todo?.completed) ? "opacity-80 line-through" : "")}>
+                        <div className="flex justify-start items-center space-x-3">
+                            <button className="p-2 rounded-md border bg-slate-800 dark:border-slate-700 dark:text-white dark:hover:bg-slate-700 dark:hover:border-slate-600" onClick={() => toggleTodo(todo?.id)}></button>
+                            <span className="text-lg">{todo?.title}</span>
+                        </div>
+                        <button className="p-2 rounded-md border bg-slate-800 dark:border-slate-700 dark:text-white dark:hover:bg-slate-700 dark:hover:border-slate-600" onClick={console.log(todo?.id)}><FaRegTrashAlt /></button>
                     </li>
                 ))}
             </ul>
--- frontend/src/partials/ToDo/Navigation.jsx
+++ frontend/src/partials/ToDo/Navigation.jsx
@@ -1,11 +1,12 @@
 import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
+import { AccountContext } from '../../contexts/UserContext';
 
 import NavItem from './NavItem'
 
 const Navigation = ({ isLoggedIn }) => {
     const [current, setCurrent] = useState('')
-    const [user, setUser] = useState('')
+    const { user } = useContext(AccountContext);
 
     const lists = ['To Do', 'In Progress', 'Done'];
 
@@ -14,7 +15,7 @@ const Navigation = ({ isLoggedIn }) => {
             <div className="p-2 h-full flex flex-col justify-between rounded-xl dark:bg-slate-900 overflow-hidden divide-y space-y-2 divide-slate-800">
                 <div className="flex flex-col justify-start divide-y divide-slate-800">
                     <div className='user flex items-center space-x-5 m-2'>
-                        <img className='w-16 border dark:border-slate-700 rounded-full' src={user?.pic || '/default.png'} alt='user profile'/>
+                        <img class='w-16 border dark:border-slate-700 rounded-full' src={user?.pic || '/default.png'} alt='user profile'/>
                         <p>{ user?.username || '(null)'}</p>
                     </div>
                     <div>
